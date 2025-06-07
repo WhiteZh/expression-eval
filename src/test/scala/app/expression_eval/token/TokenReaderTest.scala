b94348a4ce81306186d5d11b0b41e31a7df1a59c
John Bonjour <whitewolfzhang@gmail.com>
2025-06-07 10:29:03 -0700

    added more test case to TokenReaderTest

--- src/test/scala/app/expression_eval/token/TokenReaderTest.scala
+++ src/test/scala/app/expression_eval/token/TokenReaderTest.scala
@@ -17,9 +17,16 @@ class TokenReaderTest extends AnyFunSuiteLike:
     given Conversion[Int, Token.Number] with
         override def apply(x: Int): Token.Number = N(x)
 
-    test("testParsed"):
-        val tokenReader                 = TokenReader("  ( 1+2) *3 /  4 - 1  ".iterator)
-        val parsedTokens                = tokenReader.parsed
-        val supposedTokens: List[Token] = List(Bs, 1, P, 2, Be, T, 3, D, 4, M, 1)
+    test("test1"):
+        val tokenReader    = TokenReader("  ( 1+2) *3 /  4 - 1  ".iterator)
+        val parsedTokens   = tokenReader.parsed
+        val supposedTokens = List[Token](Bs, 1, P, 2, Be, T, 3, D, 4, M, 1)
+
+        assert(parsedTokens == Right(supposedTokens))
+
+    test("test2"):
+        val tokenReader    = TokenReader("(4 / 3) * (5 + (6 - 7) - ((6 + (7) * 2)))".iterator)
+        val parsedTokens   = tokenReader.parsed
+        val supposedTokens = List[Token](Bs, 4, D, 3, Be, T, Bs, 5, P, Bs, 6, M, 7, Be, M, Bs, Bs, 6, P, Bs, 7, Be, T, 2, Be, Be, Be)
 
         assert(parsedTokens == Right(supposedTokens))
