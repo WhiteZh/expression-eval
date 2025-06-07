dc7d887995a80f1a0c04acd114eaaf38a8a8c1da
John Bonjour <whitewolfzhang@gmail.com>
2025-06-07 09:55:57 -0700

    now in a more ideal logic

--- src/main/scala/app/expression_eval/ast/ExpressionNode.scala
+++ src/main/scala/app/expression_eval/ast/ExpressionNode.scala
@@ -1,6 +1,8 @@
 package app.expression_eval.ast
 
 import app.expression_eval.NumericType
+import app.expression_eval.token.Token
+import lib.util.ExplicitStatePassing.*
 
 trait ExpressionNode:
     def evaluate: NumericType
@@ -9,14 +11,133 @@ object ExpressionNode:
     case class NumberNode(x: NumericType) extends ExpressionNode:
         override def evaluate: NumericType = x
 
-    case class UnaryOperatorNode(op: NumericType => NumericType,
+    object NumberNodeParser extends PartialParser[NumberNode]:
+        override def partialParse(tokens: List[Token]): Option[(NumberNode, List[Token])] =
+            tokens match
+                case Token.Number(x) :: restTokens => Some(NumberNode(x), restTokens)
+                case _                             => None
+
+    private type UnaryFunc = NumericType => NumericType
+
+    case class UnaryOperatorNode(op: UnaryFunc,
                                  a:  ExpressionNode) extends ExpressionNode:
         override def evaluate: NumericType = op(a.evaluate)
 
-    case class BinaryOperatorNode(op: (NumericType, NumericType) => NumericType,
+    object UnaryOperatorNodeParser extends PartialParser[UnaryOperatorNode]:
+        private enum UnaryToken(val unaryFunction: UnaryFunc):
+            case Plus extends UnaryToken(a => a)
+            case Minus extends UnaryToken(a => -a)
+
+        private object UnaryToken:
+            def unapply(token: Token): Option[UnaryToken] = token match
+                case Token.Plus  => Some(Plus)
+                case Token.Minus => Some(Minus)
+                case _           => None
+
+            def unapply(unaryToken: UnaryToken): UnaryFunc = unaryToken.unaryFunction
+
+        private val aNodeParsers = List(NumberNodeParser,
+                                        UnaryOperatorNodeParser,
+                                        BracketNodeParser)
+
+        override def partialParse(tokens: List[Token]): Option[(UnaryOperatorNode, List[Token])] =
+            tokens match
+                case UnaryToken(unaryToken) :: restTokens =>
+                    aNodeParsers.iterator
+                                .map { parser => parser.partialParse(restTokens) }
+                                .collectFirst:
+                                    case Some(aNode, restTokens) => (UnaryOperatorNode(unaryToken.unaryFunction, aNode), restTokens)
+
+                case _ => None
+
+    private type BinaryFunc = (NumericType, NumericType) => NumericType
+
+    case class BinaryOperatorNode(op: BinaryFunc,
                                   a:  ExpressionNode,
                                   b:  ExpressionNode) extends ExpressionNode:
         override def evaluate: NumericType = op(a.evaluate, b.evaluate)
 
+    object BinaryOperatorNodeParser extends PartialParser[BinaryOperatorNode]:
+        private enum BinaryToken(val precedence: Int, val binaryFunction: BinaryFunc):
+            case Plus extends BinaryToken(1, (a, b) => a + b)
+            case Minus extends BinaryToken(1, (a, b) => a - b)
+            case Times extends BinaryToken(2, (a, b) => a * b)
+            case Div extends BinaryToken(2, (a, b) => a / b)
+
+        private object BinaryToken:
+            def unapply(token: Token): Option[BinaryToken] = token match
+                case Token.Plus  => Some(Plus)
+                case Token.Minus => Some(Minus)
+                case Token.Times => Some(Times)
+                case Token.Div   => Some(Div)
+                case _           => None
+
+            def unapply(binaryToken: BinaryToken): (Int, BinaryFunc) =
+                (binaryToken.precedence, binaryToken.binaryFunction)
+
+        private val operandParsers = List(NumberNodeParser,
+                                          UnaryOperatorNodeParser,
+                                          BracketNodeParser)
+
+        private def nextOperand(tokens: List[Token]): Option[(ExpressionNode, List[Token])] =
+            operandParsers.iterator
+                          .map { parser => parser.partialParse(tokens) }
+                          .collectFirst:
+                              case Some(result) => result
+
+        private def nextOperatorAndOperand(tokens: List[Token]): Option[(BinaryToken, ExpressionNode, List[Token])] =
+            tokens match
+                case BinaryToken(binaryToken) :: restTokens =>
+                    nextOperand(restTokens).map { (operand, restTokens) => (binaryToken, operand, restTokens) }
+
+                case _ => None
+
+        override def partialParse(tokens: List[Token]): Option[(BinaryOperatorNode, List[Token])] =
+            for
+                (headOperand, restTokens) <- nextOperand(tokens)
+                (binaryToken, nextOperand, restTokens) <- nextOperatorAndOperand(restTokens)
+                result <- stateLoop(headOperand: ExpressionNode, binaryToken: BinaryToken, nextOperand: ExpressionNode, restTokens):
+                    case (headOperand, binaryToken, nextOperand, Nil) =>
+                        returnResult:
+                            Some(BinaryOperatorNode(binaryToken.binaryFunction, headOperand, nextOperand), Nil)
+
+                    case (headOperand, binaryToken, nextOperand, restTokens) =>
+                        nextOperatorAndOperand(restTokens) match
+                            case Some(secondaryBinaryToken, secondaryNextOperand, restTokens) =>
+                                if secondaryBinaryToken.precedence > binaryToken.precedence then
+                                    updateState(headOperand,
+                                                binaryToken,
+                                                BinaryOperatorNode(secondaryBinaryToken.binaryFunction,
+                                                                   nextOperand,
+                                                                   secondaryNextOperand): ExpressionNode,
+                                                restTokens)
+                                else
+                                    updateState(BinaryOperatorNode(binaryToken.binaryFunction,
+                                                                   headOperand,
+                                                                   nextOperand),
+                                                secondaryBinaryToken,
+                                                secondaryNextOperand,
+                                                restTokens)
+
+                            case None => returnResult(None)
+            yield result
+
     case class BracketNode(node: ExpressionNode) extends ExpressionNode:
         override def evaluate: NumericType = node.evaluate
+
+    object BracketNodeParser extends PartialParser[BracketNode]:
+        private val innerBracketParsers: List[PartialParser[ExpressionNode]] = List(NumberNodeParser,
+                                                                                    UnaryOperatorNodeParser,
+                                                                                    BinaryOperatorNodeParser,
+                                                                                    BracketNodeParser)
+
+        override def partialParse(tokens: List[Token]): Option[(BracketNode, List[Token])] = tokens match
+            case Token.BracketStart :: restTokens =>
+                innerBracketParsers
+                    .iterator
+                    .map { parser => parser.partialParse(restTokens) }
+                    .collectFirst:
+                        case Some(innerBracket, Token.BracketEnd :: restTokens) =>
+                            (BracketNode(innerBracket), restTokens)
+
+            case _ => None
--- src/main/scala/app/expression_eval/ast/ExpressionParser.scala
+++ src/main/scala/app/expression_eval/ast/ExpressionParser.scala
@@ -1,145 +1,22 @@
 package app.expression_eval.ast
 
+import app.expression_eval.ParseError
 import app.expression_eval.token.Token
-import app.expression_eval.{NumericType, ParseError}
-import lib.util.ExplicitStatePassing.*
-
-import scala.collection.mutable.ListBuffer
-import scala.util.control.TailCalls.*
 
 object ExpressionParser extends Parser[ExpressionNode]:
-    private enum Element:
-        case Number(value: NumericType)
-        case OperatorPlus
-        case OperatorMinus
-        case OperatorTimes
-        case OperatorDiv
-        case Bracket(elements: List[Element])
 
-    private object Element:
-        type UnaryOpType = OperatorPlus.type | OperatorMinus.type
-        type BinaryOpType = OperatorPlus.type | OperatorMinus.type | OperatorTimes.type | OperatorDiv.type
+    import ExpressionNode.{BinaryOperatorNodeParser, BracketNodeParser, NumberNodeParser, UnaryOperatorNodeParser}
 
-    private object ElementListParser:
-
-        private val MissingEnclosedParenthesis = ParseError.SyntaxError("missing enclosed parenthesis")
-
-        private type OneToOneToken = Token.Number |
-                                     Token.Plus.type |
-                                     Token.Minus.type |
-                                     Token.Times.type |
-                                     Token.Div.type
-
-        extension (token: OneToOneToken)
-            private def toElement: Element = token match
-                case Token.Number(x) => Element.Number(x)
-                case Token.Plus      => Element.OperatorPlus
-                case Token.Minus     => Element.OperatorMinus
-                case Token.Times     => Element.OperatorTimes
-                case Token.Div       => Element.OperatorDiv
-
-        private def partialParse
-            (buffer: ListBuffer[Element], tokens: List[Token])
-            (insideBracket: Boolean)
-        : TailRec[Either[ParseError, (ListBuffer[Element], List[Token])]] =
-            tokens match
-                case (token: OneToOneToken) :: resTokens => tailcall(partialParse(buffer :+ token.toElement, resTokens)(insideBracket))
-
-                case Token.BracketStart :: resTokens =>
-                    partialParse(ListBuffer.empty, resTokens)(true).result match
-                        case Left(x)                       => done(Left(x))
-                        case Right(innerBuffer, resTokens) => tailcall(partialParse(buffer :+ Element.Bracket(innerBuffer.toList), resTokens)(insideBracket))
-
-                case Token.BracketEnd :: resTokens => done(if insideBracket then Right((buffer, resTokens))
-                                                           else Left(ParseError.UnexpectedTokens(Token.BracketEnd :: resTokens)))
-                case Nil                           => done(if insideBracket then Left(MissingEnclosedParenthesis)
-                                                           else Right((buffer, Nil)))
-
-        def parse(tokens: List[Token]): Either[ParseError, List[Element]] =
-            partialParse(ListBuffer.empty, tokens)(false).result.flatMap:
-                case (elements, Nil) => Right(elements.toList)
-                case (_, tokens)     => Left(ParseError.UnexpectedTokens(tokens))
-
-    private object OperatorChain:
-        type OpType = (NumericType, NumericType) => NumericType
-        type BodyLike = (Int, OpType, ExpressionNode)
-
-        def fromSeq(a: ExpressionNode, seq: Seq[BodyLike]): OperatorChain =
-            val bodySeq = seq.foldRight(None: Option[Body]):
-                (body, next) =>
-                    val (precedence: Int, op: OpType, b: ExpressionNode) = body
-                    Some(Body(precedence, op, b, next))
-
-            OperatorChain(Head(a, bodySeq))
-
-        private case class Head(a: ExpressionNode, next: Option[Body] = None)
-
-        private case class Body(precedence: Int, op: OpType, b: ExpressionNode, next: Option[Body] = None)
-
-    private class OperatorChain private(private val head: OperatorChain.Head):
-
-        import OperatorChain.{Body, Head}
-
-        def toNode: ExpressionNode = head.next match
-            case None       => head.a
-            case Some(next) => stateLoop(head.a, next):
-                (a, body) =>
-                    body.next match
-                        case None       => returnResult(ExpressionNode.BinaryOperatorNode(body.op, a, body.b))
-                        case Some(next) => if body.precedence >= next.precedence
-                                           then updateState(ExpressionNode.BinaryOperatorNode(body.op, a, body.b), next)
-                                           else updateState(a, Body(body.precedence,
-                                                                    body.op,
-                                                                    ExpressionNode.BinaryOperatorNode(next.op, body.b, next.b),
-                                                                    next.next))
-
-    private object OperatorChainParser:
-
-        import Element.*
-
-        extension (op: UnaryOpType)
-            private def buildNodeWith(value: ExpressionNode): ExpressionNode = op match
-                case OperatorPlus  => ExpressionNode.UnaryOperatorNode(identity, value)
-                case OperatorMinus => ExpressionNode.UnaryOperatorNode(-_, value)
-
-        private def compressUnaryOps(ops: Iterable[UnaryOpType], value: ExpressionNode): ExpressionNode =
-            ops.foldLeft(value)((node, op) => op.buildNodeWith(node))
-
-        private def takeFirstNode(elements: List[Element]): Either[ParseError, (ExpressionNode, List[Element])] =
-            stateLoop(ListBuffer.empty: ListBuffer[UnaryOpType], elements) {
-                case (buffer, Number(num) :: restList)            => returnResult(Right((compressUnaryOps(buffer, ExpressionNode.NumberNode(num)), restList)))
-                case (buffer, Bracket(innerElements) :: restList) => returnResult:
-                    val inner = OperatorChainParser.parse(innerElements)
-                    inner.map { innerOPC => (compressUnaryOps(buffer, innerOPC.toNode), restList) }
-                case (buffer, (unaryOp: UnaryOpType) :: restList) => updateState(buffer :+ unaryOp, restList)
-                case (_, _ :: _)                                  => returnResult(Left(ParseError.SyntaxError()))
-                case (_, Nil)                                     => returnResult(Left(ParseError.SyntaxError()))
-            }
-
-        extension (op: BinaryOpType)
-            private def buildBodyLikeWith(node: ExpressionNode): OperatorChain.BodyLike = op match
-                case OperatorPlus  => (1, _ + _, node)
-                case OperatorMinus => (1, _ - _, node)
-                case OperatorTimes => (2, _ * _, node)
-                case OperatorDiv   => (2, _ / _, node)
-
-        def parse(elements: List[Element]): Either[ParseError, OperatorChain] =
-            takeFirstNode(elements).flatMap:
-                (head, tailElements) =>
-                    stateLoop(ListBuffer.empty: ListBuffer[OperatorChain.BodyLike], tailElements):
-                        case (buffer, Nil)                                => returnResult(Right(OperatorChain.fromSeq(head, Seq.empty)))
-                        case (buffer, (op: BinaryOpType) :: tailElements) => takeFirstNode(tailElements) match
-                            case Left(e)                   => returnResult(Left(e))
-                            case Right(node, Nil)          => returnResult:
-                                val bodyLikeList  = (buffer :+ op.buildBodyLikeWith(node)).toList
-                                val operatorChain = OperatorChain.fromSeq(head, bodyLikeList)
-                                Right(operatorChain)
-                            case Right(node, tailElements) => updateState(buffer :+ op.buildBodyLikeWith(node), tailElements)
-
-                        case _ => returnResult(Left(ParseError.SyntaxError()))
+    private val parsers = List(NumberNodeParser,
+                               UnaryOperatorNodeParser,
+                               BinaryOperatorNodeParser,
+                               BracketNodeParser)
 
     override def parse(tokens: List[Token]): Either[ParseError, ExpressionNode] =
-        for
-            elementList <- ElementListParser.parse(tokens)
-            operatorChain <- OperatorChainParser.parse(elementList)
-        yield operatorChain.toNode
+        parsers.iterator
+               .map { parser => parser.parse(tokens) }
+               .collectFirst:
+                   case Right(node) => node
+        match
+            case Some(node) => Right(node)
+            case None       => Left(ParseError.NoMatch)
